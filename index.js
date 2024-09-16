a54bcd72f26603dfd0d1b20083b1244e9aac9ca6
azamatqahramonov <azamatqahramonov9@gmail.com>
2024-09-16 23:08:45 +0500

    begin 3

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = +prompt("A sonni kiriting"); 
+let b = +prompt("B sonni kiriting"); 
+
+let S = a * b; 
+let P = 2 * (a + b); 
+
+console.log(typeof a);
+console.log(typeof b);
+
+alert("To'rtburchakning yuzasi: " + S + "\nTo'rtburchakning perimetri: " + P); 
